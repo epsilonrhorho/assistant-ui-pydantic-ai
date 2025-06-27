a0c4e60155d94e403494b68d1079ba8d8b1fdf9f
Stefan Arentz <stefan@arentz.ca>
2025-06-26 21:37:26 -0400

    Frontend: Talk to our Backend instead of OpenAI

--- frontend/app/api/chat/route.ts
+++ frontend/app/api/chat/route.ts
@@ -1,15 +1,20 @@
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 import { frontendTools } from "@assistant-ui/react-ai-sdk";
 import { streamText } from "ai";
 
 export const runtime = "edge";
 export const maxDuration = 30;
 
+const localOpenAI = createOpenAI({
+  apiKey: "does-not-matter",
+  baseURL: "http://127.0.0.1:8000/v1",
+});
+
 export async function POST(req: Request) {
   const { messages, system, tools } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: localOpenAI("does-not-matter"), // The model doesn't really matter since it is configured in the backend
     messages,
     // forward system prompt and tools from the frontend
     toolCallStreaming: true,
